9f92a523f9e84fbffe04af4f6c2292ac10b1aa86
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-12-24 12:34:55 -0500

    Add mismatch leaves allowed structure tag
    Any structure in this tag will be allowed to have leaves that do not match the logs. This allows players and other modders to tag structures where mismatched leaves/logs are intended and should not decay.
    
    Name of tag can be changed. I added two bumblezone entries in the tag as an example. You can keep the entries or remove them. The tag entries are optional tag entries so they do not error even if bumblezone is not on.
    
    Fixes https://github.com/TeamDiopside/SeparatedLeaves/issues/4

--- common/src/main/java/nl/teamdiopside/separatedleaves/mixin/LeavesBlockMixin.java
+++ common/src/main/java/nl/teamdiopside/separatedleaves/mixin/LeavesBlockMixin.java
@@ -2,12 +2,16 @@ package nl.teamdiopside.separatedleaves.mixin;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
 import nl.teamdiopside.separatedleaves.Reload;
+import nl.teamdiopside.separatedleaves.SeparatedLeaves;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,6 +25,15 @@ public abstract class LeavesBlockMixin {
 
     @Inject(method = "updateDistance", at = @At("HEAD"), cancellable = true)
     private static void updateDistance(BlockState blockState, LevelAccessor levelAccessor, BlockPos blockPos, CallbackInfoReturnable<BlockState> cir) {
+        // Skip logic for structures that are tagged as allowing mismatched leaves
+        if (levelAccessor instanceof ServerLevel serverLevel) {
+            StructureManager structureManager = serverLevel.structureManager();
+            StructureStart detectedStructure = structureManager.getStructureWithPieceAt(blockPos, SeparatedLeaves.ALLOW_MISMATCHED_LEAVES_STRUCTURES);
+            if (detectedStructure.isValid()) {
+                return;
+            }
+        }
+
         boolean hasFile = false;
         int i = 7;
         BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
