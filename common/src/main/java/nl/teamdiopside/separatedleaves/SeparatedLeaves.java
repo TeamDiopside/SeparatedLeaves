8cfef8c048120592acbfa5f79cbbed31d5c44584
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-12-24 12:34:55 -0500

    Add mismatch leaves allowed structure tag
    Any structure in this tag will be allowed to have leaves that do not match the logs. This allows players and other modders to tag structures where mismatched leaves/logs are intended and should not decay.
    
    Name of tag can be changed. I added two bumblezone entries in the tag as an example. You can keep the entries or remove them. The tag entries are optional tag entries so they do not error even if bumblezone is not on.
    
    Fixes https://github.com/TeamDiopside/SeparatedLeaves/issues/4

--- common/src/main/java/nl/teamdiopside/separatedleaves/SeparatedLeaves.java
+++ common/src/main/java/nl/teamdiopside/separatedleaves/SeparatedLeaves.java
@@ -1,12 +1,17 @@
 package nl.teamdiopside.separatedleaves;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.levelgen.structure.Structure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SeparatedLeaves {
     public static final String MOD_ID = "separatedleaves";
     public static final Logger LOGGER = LoggerFactory.getLogger("Separated Leaves");
+    public static final TagKey<Structure> ALLOW_MISMATCHED_LEAVES_STRUCTURES = TagKey.create(Registries.STRUCTURE, new ResourceLocation(MOD_ID, "allow_mismatched_leaves_structures"));
 
     public static MinecraftServer minecraftServer = null;
     
