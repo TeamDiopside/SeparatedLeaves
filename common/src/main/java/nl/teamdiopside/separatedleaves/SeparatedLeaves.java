ef6892e7d26e875d636794a5b13193b881be36e5
LarsMans <l.s.vanemmerik@gmail.com>
2023-12-25 13:22:23 +0100

    fix registry

--- common/src/main/java/nl/teamdiopside/separatedleaves/SeparatedLeaves.java
+++ common/src/main/java/nl/teamdiopside/separatedleaves/SeparatedLeaves.java
@@ -1,6 +1,6 @@
 package nl.teamdiopside.separatedleaves;
 
-import net.minecraft.core.registries.Registries;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tags.TagKey;
@@ -12,8 +12,8 @@ import org.slf4j.LoggerFactory;
 public class SeparatedLeaves {
     public static final String MOD_ID = "separatedleaves";
     public static final Logger LOGGER = LoggerFactory.getLogger("Separated Leaves");
-    public static final TagKey<Structure> ALLOW_MISMATCHED_LEAVES_STRUCTURES = TagKey.create(Registries.STRUCTURE, new ResourceLocation(MOD_ID, "allow_mismatched_leaves"));
-    public static final TagKey<Biome> ALLOW_MISMATCHED_LEAVES_BIOMES = TagKey.create(Registries.BIOME, new ResourceLocation(MOD_ID, "allow_mismatched_leaves"));
+    public static final TagKey<Structure> ALLOW_MISMATCHED_LEAVES_STRUCTURES = TagKey.create(Registry.STRUCTURE_REGISTRY, new ResourceLocation(MOD_ID, "allow_mismatched_leaves"));
+    public static final TagKey<Biome> ALLOW_MISMATCHED_LEAVES_BIOMES = TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(MOD_ID, "allow_mismatched_leaves"));
 
     public static MinecraftServer minecraftServer = null;
     
