f1bc69cdf56d400b9d2108f7a83ea6d4f7347e38
Curry <66020230+curryducker@users.noreply.github.com>
2025-03-10 21:22:26 +0100

    reintroduce biome tag

--- common/src/main/java/nl/teamdiopside/separatedleaves/SeparatedLeaves.java
+++ common/src/main/java/nl/teamdiopside/separatedleaves/SeparatedLeaves.java
@@ -4,6 +4,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ public class SeparatedLeaves {
     public static final String MOD_ID = "separatedleaves";
     public static final Logger LOGGER = LoggerFactory.getLogger("Separated Leaves");
     public static final TagKey<Structure> ALLOW_MISMATCHED_LEAVES_STRUCTURES = TagKey.create(Registries.STRUCTURE, ResourceLocation.fromNamespaceAndPath(MOD_ID, "allow_mismatched_leaves"));
+    public static final TagKey<Biome> ALLOW_MISMATCHED_LEAVES_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(MOD_ID, "allow_mismatched_leaves"));
 
     public static MinecraftServer minecraftServer = null;
     
